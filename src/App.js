f578a431d9e93d1f7dce3cb0b4a604cf16398306
unknown <himanshu25031991@gmail.com>
2020-09-29 12:12:11 +0530

    withRouter()

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route  path="/hats" component={HatsPage} />
       </Switch>
     </div>
   );

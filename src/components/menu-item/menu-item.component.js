9cc012f64d543500a6e08f474cc2cadbcc6208e0
unknown <himanshu25031991@gmail.com>
2020-09-29 12:13:41 +0530

    withRouter() closes #10

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
-
+//added history, link, match for routing
 const MenuItem = (props) => {
   const { title, imageUrl, size, history, linkUrl, match } = props;
   return (
